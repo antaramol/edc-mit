aefd500a99b6c90158217a91a8ae623d7a6ace8d
Antonj <antonj.arag99@gmail.com>
2022-01-23 23:26:37 +0100

    Cambios

--- trabajo/tb_top.vhd
+++ trabajo/tb_top.vhd
@@ -33,8 +33,7 @@ architecture bench of top_level_tb is
   signal y_valid : std_logic;
   signal estim : complex10;
   signal estim_valid : std_logic;
-  signal portadora_s : real;
-  signal y_re, y_im :signed (DATA_WIDTH/2-1 downto 0);
+  signal y_re_s, y_im_s : std_logic_vector(31 downto 0);
  
 
 begin
@@ -57,9 +56,14 @@ begin
     -- variable portadoras_re, portadoras_im : integer_array_t;
        
     variable i : integer;
-    file input_file : text open read_mode is "../Matlab/portadoras_im.csv";
-    variable input_line : line;
-    variable portadora : real;
+    -- file input_file_re : text open read_mode is "../Matlab/portadoras_re.csv";
+    -- file input_file_im : text open read_mode is "../Matlab/portadoras_im.csv";
+
+    -- variable input_line_re, input_line_im : line;
+    -- variable portadora_re, portadora_im : real;
+
+    variable portadora_re, portadora_im : integer_array_t;
+    variable y_re, y_im : std_logic_vector(31 downto 0);
 
   begin
     
@@ -71,14 +75,26 @@ begin
         wait for clk_period;
         rst <= '0';
 
-        readline(input_file, input_line);
-        read(input_line, portadora);
-        portadora_s <= portadora;
+        portadora_re := load_csv("../Matlab/portadoras_re.csv");
+        portadora_im := load_csv("../Matlab/portadoras_im.csv");
+        
+        i := 0;
+        while i < length(portadora_re) loop --tienen la misma longitud
+          y_re := std_logic_vector(to_signed(get(portadora_re,i),32));
+          
+          y_im := std_logic_vector(to_signed(get(portadora_im,i),32));
 
-        wait for 10 * clk_period;
-        readline(input_file, input_line);
-        read(input_line, portadora);
-        portadora_s <= portadora;
+          y<= (DATA_WIDTH-1 downto DATA_WIDTH/2 => y_re(31 downto 21), DATA_WIDTH/2-1 downto 0 => y_im(31 downto 21)); --Cogemos los 10 primeros bits, a la salida habría que
+          -- añadir ceros al final hasta completar los 32 bits del integer, y dividir por 10e8
+         
+          y_re_s <= y_re; -- Para verlo a la salida
+          y_im_s <= y_im; 
+
+          wait for clk_period;
+          i := i+1;
+        end loop;
+
+
         wait for 100*clk_period;
 
       end if;
