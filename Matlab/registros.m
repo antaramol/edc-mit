96563cec9afc75e0cc84ddef41f12df9e1fbc8be
Antonio Aragon <antonj.arag99@gmail.com>
2022-02-03 12:49:24 +0100

    Hecho función Para poder leerlo desde python

--- Matlab/registros.m
+++ Matlab/registros.m
@@ -1,14 +1,10 @@
 %% Generación secuencia PRBS
+function pilotos = registros(N_portadoras)
+    registro = ones(1,11);
+    pilotos = zeros(N_portadoras,1);
+    for n = 1:N_portadoras
+        pilotos(n,:) = 4/3*2*(0.5-registro(end));
+        registro = [xor(registro(end),registro(end-2)),registro(1:(end-1))];
+    end
 
-clear
-
-registro = ones(1,11);
-registro_salida = zeros(1,143);
-for n = 1:143
-    % registro_salida = [registro(end),registro_salida(1:end-1)];
-    registro_salida(n) = registro(end);
-    registro = [xor(registro(end),registro(end-2)),registro(1:(end-1))];
 end
-
-% Los pilotos tienen amplitud +-4/3
-registro_salida = 4/3*2*(0.5-registro_salida);
