71fcca53d29601aad861cff7e09b1d85250ffc25
Antonio Aragon <antonj.arag99@gmail.com>
2022-02-10 23:07:53 +0100

    Inserción comentario

--- trabajo/Matlab/registro.m
+++ trabajo/Matlab/registro.m
@@ -1,4 +1,5 @@
 %% Generación secuencia PRBS
+% Escrito a modo de función, script diseñado para pruebas
 function pilotos = registro(N_portadoras)
     registro = ones(1,11);
     pilotos = zeros(N_portadoras,1);
@@ -6,5 +7,4 @@ function pilotos = registro(N_portadoras)
         pilotos(n,:) = 4/3*2*(0.5-registro(end));
         registro = [xor(registro(end),registro(end-2)),registro(1:(end-1))];
     end
-
 end
